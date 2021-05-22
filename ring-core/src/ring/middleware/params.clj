aeecadb8e88549ea99fa7a46e7a865dc359196bd
bsless <ben.sless@gmail.com>
2021-05-22 18:31:28 +0300

    Unroll params parsing
    Replace merge-with merge with assoc and faster merge
    
    Faster merge is achieved by two means:
    1. Using transients and reduce-kv between only two maps
    2. Short circuiting missing values or empty maps

--- ring-core/src/ring/middleware/params.clj
+++ ring-core/src/ring/middleware/params.clj
@@ -8,25 +8,49 @@
   (let [params (codec/form-decode params encoding)]
     (if (map? params) params {})))
 
+(defn fast-merge*
+  [x y]
+  (persistent! (reduce-kv assoc! (transient x) y)))
+
+(defn fast-merge
+  [x y]
+  (let [nx (count x)
+        ny (count y)]
+    (if (zero? nx)
+      (if (zero? ny) {} y)
+      (if (zero? ny)
+        x
+        (fast-merge* x y)))))
+
 (defn assoc-query-params
   "Parse and assoc parameters from the query string with the request."
   {:added "1.3"}
   [request encoding]
-  (merge-with merge request
+  (let [params (:params request)
+        query-params (:query-params request)]
     (if-let [query-string (:query-string request)]
-      (let [params (parse-params query-string encoding)]
-        {:query-params params, :params params})
-      {:query-params {}, :params {}})))
+      (let [parsed-params (parse-params query-string encoding)]
+        (-> request
+            (assoc :query-params (fast-merge query-params parsed-params))
+            (assoc :params (fast-merge params parsed-params))))
+      (cond-> request
+        (nil? query-params) (assoc :query-params {})
+        (nil? params) (assoc :params {})))))
 
 (defn assoc-form-params
   "Parse and assoc parameters from the request body with the request."
   {:added "1.2"}
   [request encoding]
-  (merge-with merge request
+  (let [params (:params request)
+        form-params (:form-params request)]
     (if-let [body (and (req/urlencoded-form? request) (:body request))]
-      (let [params (parse-params (slurp body :encoding encoding) encoding)]
-        {:form-params params, :params params})
-      {:form-params {}, :params {}})))
+      (let [parsed-params (parse-params (slurp body :encoding encoding) encoding)]
+        (-> request
+            (assoc :form-params (fast-merge form-params parsed-params))
+            (assoc :params (fast-merge params parsed-params))))
+      (cond-> request
+        (nil? form-params) (assoc :form-params {})
+        (nil? params) (assoc :params {})))))
 
 (defn params-request
   "Adds parameters from the query string and the request body to the request
